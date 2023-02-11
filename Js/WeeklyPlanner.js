05f581c10f71314d1ca14f6b016d94933a16bf03
Viktor Beckes <viktorb9772@gmail.com>
2023-02-11 15:18:18 +0100

    IdidWhatICould

--- Js/WeeklyPlanner.js
+++ Js/WeeklyPlanner.js
@@ -1,115 +1,107 @@
-let plans = `[{
+let plans = `[{ 
     "title" : "Take the dog for a walk",
-    "description" : "Every day, 2 times, morning and afternoon",
-    
+    "about" : "Every day, 2 times, morning and afternoon",
     "priority" : "0",
-    "pic" : "Images/walk.jpg"
+    "picture" : "Images/walk.jpg"
 },{
     "title" : "buy groceries",
-    "description" : "Running outta food!",
-    
+    "about" : "Running outta food!",
     "priority" : "0",
-    "pic" : "Images/groceries.jpg"
+    "picture" : "Images/groceries.jpg"
 },{
     "title" : "Find new job",
-    "description" : "Cant keep working as a hitman",
-    
+    "about" : "Cant keep working as a hitman",
     "priority" : "0",
-    "pic" : "Images/job.jpg"
+    "picture" : "Images/job.jpg"
 },{
     "title" : "Do Code Review 2",
-    "description" : "For the future job",
-    
+    "about" : "For the future job",
     "priority" : "0",
-    "pic" : "Images/code.jpg"
+    "picture" : "Images/code.jpg"
 },{
     "title" : "Kill Joe",
-    "description" : "Next hit",
-    
+    "about" : "Next hit",
     "priority" : "0",
-    "pic" : "Images/murder.jpg"
+    "picture" : "Images/murder.jpg"
 },{
     "title" : "Hide Body",
-    "description" : "Joes pretty heavy, have to cremate him",
-    
+    "about" : "Joes pretty heavy, have to cremate him",
     "priority" : "0",
-    "pic" : "Images/dead.jpg"
+    "picture" : "Images/dead.jpg"
 },{
     "title" : "Hide Gun",
-    "description" : "hide evidence",
-    
+    "about" : "hide evidence",
     "priority" : "0",
-    "pic" : "Images/gun.jpg"
+    "picture" : "Images/gun.jpg"
 },{
     "title" : "Leave The Country",
-    "description" : "Run away after becoming a FSDeveloper",
-    
+    "about" : "Run away after becoming a FSDeveloper",
     "priority" : "0",
-    "pic" : "Images/border.jpg"
+    "picture" : "Images/border.jpg"
 },{
     "title" : "Cheat On Wife",
-    "description" : "getting rid of her nagging ass",
-    
+    "about" : "getting rid of her nagging ass",
     "priority" : "0",
-    "pic" : "Images/cheat.jpg"
+    "picture" : "Images/cheat.jpg"
 }]`;
 
-plans = JSON.parse(plans);
+plans = JSON.parse(plans); //converts the PLANS in to a JSON file
 
-for(let o of plans){
+for(let o of plans){ //makes a loop that ceartes the cards remplate with bootsstrap
     document.getElementById("result").innerHTML += `
     <div class="add">
     <div class=" border border-dark card">
     <div class="spacer"></div>
     <div class="taskbtn">
     <p class="btn btn-primary">Task</p>
+    <i class="pull-right fa fa-bookmark"></i>
+    <i class="pull-right fa fa-ellipsis-v"></i>
     </div>
     <div class="container">
-    <img src="${o.pic}" class=" border border-dark card-img-top cardpic" alt="${o.title}">
+    <img src="${o.picture}" class=" border border-dark card-img-top cardpic" alt="${o.title}">
     </div>
     <div class="card-body">
      <h5 class="card-title text-center">${o.title}</h5>
-     <p class="text-center">${o.description}</p>
+     <p class="text-center">${o.about}</p>
      <hr>
     <div class="propertylevel">
-        <p class="priority fs-4">Priority Level: <span class="btn btn-success priorityBtn">${o.priority}</span</p>
+        <p class="priority fs-4"><i class="fa fa-exclamation-circle"></i> Priority Level: <span class="btn btn-success priorityBtn">${o.priority}</span</p>
         <div>
         </div>
     </div>
-     <p class="fs-5 deadline">DeadLine: 11.02.2023 18:00pm</p>
-     <hr>
-     <div class="text-center">
-     <p class="btn btn-danger delete">Delete</p>
-     <p class="btn btn-success">Done</p>
-     </div>
+    <p class="fs-5 deadline"><i class="fa fa-calendar"></i> DeadLine: 11.02.2023 18:00pm</p>
+    <hr>
+    <div class="text-center">
+    <p class="btn btn-danger delete">Delete</p>
+    <p class="btn btn-success">Done</p>
+    </div>
     </div>
     </div>
     </div>`;
 }
 
-let btns = document.getElementsByClassName("priorityBtn");
-
+let btns = document.getElementsByClassName("priorityBtn"); //button for importance 
 for(let i = 0; i < btns.length; i++){
-    btns[i].addEventListener("click", function(){
+    btns[i].addEventListener("click", function(){ //loop with of statments that increases the number in the button and changes colors
         plans[i].priority++;
         document.getElementsByClassName("priorityBtn")[i].innerHTML = plans[i].priority;
         if (plans[i].priority < 2){
-            console.log("Green");   
-            document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-warning");
+            console.log("Green");   //if the importance button is less than 2 turns green
+            document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-warning"); 
             document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-danger");
             document.getElementsByClassName("priorityBtn")[i].classList.add("btn-success");
-        }else if (plans[i].priority < 4) {
+        }else if (plans[i].priority < 4) {   //if the importance button is less than 4 turns yellow
             console.log("yellow"); 
             document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-success");
             document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-danger");
             document.getElementsByClassName("priorityBtn")[i].classList.add("btn-warning");
-        }else if (plans[i].priority < 6) {
+        }else if (plans[i].priority < 6) { //if the importance button is less than 6 turns red
             console.log("red"); 
             document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-success");
             document.getElementsByClassName("priorityBtn")[i].classList.remove("btn-warning");
              document.getElementsByClassName("priorityBtn")[i].classList.add("btn-danger");
              document.getElementsByClassName("card")[i].style.color = "red";
-        }else if (plans[i].priority >= 6) {
+        }else if (plans[i].priority >= 6) { //if the importance button is 6 or bigger makes the whole card red
             console.log("warrning"); 
             document.getElementsByClassName("card")[i].style.backgroundColor = "red";
             document.getElementsByClassName("card")[i].style.color = "black";
